9195545b3c6b9de3a13899c58d0471f3a75b18d5
RonSw <e1ndhoven>
2014-08-30 01:19:45 +0200

    Fill database with correct coordinates partially each time the main window part is loaded.

--- frontend/javascript/controllers/main.js
+++ frontend/javascript/controllers/main.js
@@ -5,14 +5,35 @@ angular.module('ribs.controller.main', ['google-maps']).controller('MainControll
     Customers.get()
       .success(function(data) {
         $scope.customers = data;
-          
-        for (var i = 0; i < data.length; i++){
         
-            if (data[i].Location[0] !== 0 && data[i].Location[1] !== 0 )
+        var nrOfSystemMarkersOnMap = 0;
+        for (var i = 0; i < data.length; i++){
+            var customer = data[i];
+            // hack to fill database..
+            if ((customer.Location[0] === 0 && customer.Location[1] === 0) ||
+               (customer.Location[0] === 40 && customer.Location[1] === 50))
+            {            
+                // now do a query
+                var hospitalName = customer.HospitalName;
+                var city = customer.City;
+                var country = customer.CountryName;
+                
+                if (country !== "" && city !== "")
+                {
+                    var placeForRequest = city + ", " + country;
+          
+                    doUpdateCoordinates(customer, placeForRequest);        
+                }
+             }      
+            else
             {
                 createMarker(data[i]);
+                nrOfSystemMarkersOnMap++;
             }
-        }        
+
+        }  
+        
+        $scope.tagline = 'For all your service and replacement requirements! Nr of systems on map:' + nrOfSystemMarkersOnMap;
     });
     };
     
@@ -30,6 +51,26 @@ angular.module('ribs.controller.main', ['google-maps']).controller('MainControll
         maxWidth: 400
     });
     
+    var doUpdateCoordinates = function (customer, placeForRequest){
+                console.log("Location empty, performing request to get location: " + placeForRequest);      
+                var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode( { 'address': placeForRequest } , function(results, status) {
+                      if (status == google.maps.GeocoderStatus.OK) {
+                        var location = results[0].geometry.location;
+                        console.log("the location is: " + location);
+                        var lat = location.lat();
+                        var longnr = location.lng();
+                        var latlangnr = new Array(lat, longnr);
+                        customer.Location = latlangnr;
+                        Customers.update(customer);
+                      } 
+                      else {
+                        console.log("Geocode was not successful for the following reason: " + status);
+                      }
+                    });        
+    
+    };
+    
     var createMarker = function (info){
         console.log(info);
         var hospitalName = info.HospitalName;
@@ -42,11 +83,10 @@ angular.module('ribs.controller.main', ['google-maps']).controller('MainControll
             title: info.HospitalName
         });
         
-        marker.content = '<div class="infoWindowContent">' + info.City + ', ' + info.CountryName +  '</br>Location: ' + info.Location + ' </div>';
+        marker.content = '<div class="infoWindowContent">' + info.City + ', ' + info.CountryName + '</div>';
         
         google.maps.event.addListener(marker, 'click', function(){
-        
-            infoWindow.setContent('<b>' + marker.title + '</b>' + marker.content);
+            infoWindow.setContent('<b>' + marker.title + '</b></BR>' + marker.content);
             infoWindow.open($scope.map, marker);
         });
         
