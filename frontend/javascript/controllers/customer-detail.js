ff8ab9a55b552ba91bf891dc94daeb7779df6df9
RonSw <e1ndhoven>
2014-08-29 22:34:00 +0200

    Updating customers with lat and long pos via google api

--- frontend/javascript/controllers/customer-detail.js
+++ frontend/javascript/controllers/customer-detail.js
@@ -4,10 +4,9 @@ angular.module('ribs.controller.customer-detail', []).controller('CustomerDetail
         $scope.customer = data;
         
         console.log('Retrieved getCustomerDetail');        
-        if (data.customer.Location[0] === 0 && data.customer.Location[1] === 0)
-        {     
-            console.log("Location empty");
-            console.log('Location empty');                
+        if ((data.customer.Location[0] === 0 && data.customer.Location[1] === 0) ||
+           (data.customer.Location[0] === 40 && data.customer.Location[1] === 50))
+        {            
             // now do a query
             var hospitalName = data.customer.HospitalName;
             var city = data.customer.City;
@@ -15,15 +14,17 @@ angular.module('ribs.controller.customer-detail', []).controller('CustomerDetail
             
             if (country !== "" && city !== "")
             {
-                console.log('Performing request to get location');
-                var placeForRequest = String(city) + ", " + String(country);
+                var placeForRequest = city + ", " + country;
+                console.log("Location empty, performing request to get location: " + placeForRequest);                
                 var geocoder = new google.maps.Geocoder();
-                geocoder.geocode( { 'address': placeForRequest}, function(results, status) {
+                geocoder.geocode( { 'address': placeForRequest } , function(results, status) {
                   if (status == google.maps.GeocoderStatus.OK) {
                     var location = results[0].geometry.location;
-                  
-                    var latlangnr = [location.latitude, location.longitude];
-                    data.customer.position = latlangnr;
+                    console.log("the location is: " + location);
+                    var lat = location.lat();
+                    var longnr = location.lng();
+                    var latlangnr = new Array(lat, longnr);
+                    data.customer.Location = latlangnr;
                     Customers.update(data.customer);
                   } 
                   else {
